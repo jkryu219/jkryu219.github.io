cab7d2686850d267792829d8607b07815ab698bc
Ryureka <starlight747@naver.com>
2020-11-04 02:43:41 +0900

    중형 디바이스 글 영역 넓게 보이도록 반응형 처리

--- assets/js/search.js
+++ assets/js/search.js
@@ -29,7 +29,7 @@
       }
 
       for (var i = 0; i < results.length; i++) { // Iterate over the results
-        console.log(results[i]);
+        // console.log(results[i]);
         var item = store[results[i].ref];
         appendString += '<ul><li class="wow fadeInLeft" data-wow-duration="1.5s" style="visibility: visible; animation-duration: 1.5s; animation-name: fadeInLeft;"><a class="zoombtn" href="' + item.url + '">' + item.title + '</a><p>' + item.excerpt + '</p><a class="btn zoombtn" href="' + item.url + '">Read More</a></li></ul>';
       }
